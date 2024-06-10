ee729546b35b22528629f461e021ff15dcc7c130
Quang Minh <quangnm2209@gmail.com>
2024-06-10 16:45:45 +0700

    adding authentication

--- src/app/route/route.js
+++ src/app/route/route.js
@@ -1,9 +1,28 @@
 import Admin from "../admin/Admin";
-
+import Login from "../auth/Login";
+import ForgotPassword from "../auth/ForgotPassword";
+import Register from "../auth/Register";
+import Home from "../home/Home";
 export const publicRouter = [
   {
+    element: Home,
+    path: "/",
+  },
+  {
+    element: Login,
+    path: "/login",
+  },
+  {
+    element: Register,
+    path: "/register",
+  },
+  {
+    element: ForgotPassword,
+    path: "/forgot_password",
+  },
+  {
     element: Admin,
-    path: "/:slug",
+    path: "/admin/:slug",
   },
 ];
 export const adminRouter = [];
