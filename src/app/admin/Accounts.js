85fec15622113719f67d9f1d3c9b418e7fc79f88
Quang Minh <quangnm2209@gmail.com>
2024-06-12 09:31:48 +0700

    creating admin account API

--- src/app/admin/Accounts.js
+++ src/app/admin/Accounts.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Pagination from "../component/pagination/Pagination";
 import AccountCard from "../component/card/AccountCard";
+import { toast } from "react-toastify";
+import AuthService from "../service/AuthService";
 const Accounts = () => {
   const [accounts, setAccounts] = useState({});
 
@@ -14,20 +16,21 @@ const Accounts = () => {
   };
 
   const [reload, setReload] = useState(false);
+  const [showAdd, setShowAdd] = useState(false);
 
-  useEffect(() => {
-    if (getQueryParam("search")) {
-      handleGetAllAccounts(
-        `page=${getQueryParam("page")}&search=${getQueryParam("search")}`
-      );
-    } else {
-      handleGetAllAccounts(`page=${getQueryParam("page")}`);
-    }
-  }, [reload, search]);
+  // useEffect(() => {
+  //   if (getQueryParam("search")) {
+  //     handleGetAllAccounts(
+  //       `page=${getQueryParam("page")}&search=${getQueryParam("search")}`
+  //     );
+  //   } else {
+  //     handleGetAllAccounts(`page=${getQueryParam("page")}`);
+  //   }
+  // }, [reload, search]);
 
-  useEffect(() => {
-    handleGetRole();
-  }, []);
+  // useEffect(() => {
+  //   handleGetRole();
+  // }, []);
 
   const handleGetAllAccounts = async (query) => {
     // try {
@@ -73,9 +76,18 @@ const Accounts = () => {
         style={{
           display: "flex",
           marginTop: "20px",
-          justifyContent: "space-between",
+          justifyContent: "flex-end",
         }}
-      ></div>
+      >
+        <button
+          onClick={() => {
+            setShowAdd(true);
+          }}
+          className="btn btn-primary"
+        >
+          Thêm mới tk admin
+        </button>
+      </div>
       <section style={{ marginTop: "15px" }} className="ftco-section">
         <div className="row">
           <div className="col-md-12">
@@ -103,6 +115,149 @@ const Accounts = () => {
         </div>
       </section>
       {accounts?.accounts && <Pagination count={accounts?.total} />}
+      {showAdd && <AddNewAccount setShow={setShowAdd} />}
+    </div>
+  );
+};
+const AddNewAccount = ({ setShow }) => {
+  const [err, setErr] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleCreateNewAccount = async () => {
+    try {
+      const user = {
+        username: usernameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      };
+      const userFeildName = {
+        username: "Tên",
+        email: "Email",
+        password: "Mật khẩu",
+      };
+      let isErr = false;
+      let newError = {};
+      for (const [key, value] of Object.entries(user)) {
+        if (!user[key]) {
+          isErr = true;
+          newError[key] = userFeildName[key] + " không được bỏ trống!";
+        }
+      }
+      setErr(newError);
+
+      if (!isErr) {
+        const data = await AuthService.createAccount(user);
+        toast.success(data?.data?.Message);
+        setShow(false);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.response?.data?.Error);
+    }
+  };
+
+  return (
+    <div className="create_container">
+      <div className="create_wrap">
+        <div
+          onClick={() => {
+            setShow(false);
+          }}
+          className="create_close_wrap"
+        >
+          <i className="fa-solid fa-x"></i>
+        </div>
+        <div style={{ marginBottom: "20px", marginTop: "30px" }}>
+          {err?.username && (
+            <div className="create_input_container">
+              <i
+                style={{
+                  color: "red",
+                  marginBottom: "0px",
+                  marginTop: "15px",
+                }}
+              >
+                {err?.username}
+              </i>
+            </div>
+          )}
+          <div className="create_input_container">
+            <input
+              ref={usernameRef}
+              placeholder="Tên *"
+              className="create_input"
+              name="text"
+            />
+          </div>
+          {err?.email && (
+            <div className="create_input_container">
+              <i
+                style={{
+                  color: "red",
+                  marginBottom: "-20px",
+                  marginTop: "15px",
+                }}
+              >
+                {err?.email}
+              </i>
+            </div>
+          )}
+          <div style={{ marginTop: "20px" }} className="create_input_container">
+            <input
+              ref={emailRef}
+              placeholder="Email *"
+              className="create_input"
+              name="email"
+            />
+          </div>
+          {err?.password && (
+            <div className="create_input_container">
+              <i
+                style={{
+                  color: "red",
+                  marginBottom: "-20px",
+                  marginTop: "15px",
+                }}
+              >
+                {err?.password}
+              </i>
+            </div>
+          )}
+          <div style={{ marginTop: "20px" }} className="create_input_container">
+            <input
+              ref={passwordRef}
+              placeholder="Mật khẩu *"
+              className="create_input"
+              type="password"
+            />
+          </div>
+          <div className="btn_create_container">
+            <button
+              onClick={handleCreateNewAccount}
+              style={{ margin: "0 5px" }}
+              className="btn btn-primary"
+            >
+              Tạo mới
+            </button>
+            <button
+              onClick={() => {
+                setShow(false);
+              }}
+              style={{ margin: "0 5px" }}
+              className="btn btn-secondary"
+            >
+              Hủy
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
